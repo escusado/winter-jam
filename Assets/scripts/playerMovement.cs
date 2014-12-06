b8792dcb39cf9c6715b89ecd770cc2a8819f9751
Toily <escusado@gmail.com>
2014-12-06 15:08:18 -0600

    add player and controls

---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class playerMovement : MonoBehaviour {
+
+	public float speed;
+	public Boundary boundary;
+
+	private float prevX;
+	private float prevY;
+	private Vector3 currentMovement;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		var inputDevice = InputManager.ActiveDevice;
+		
+		float moveHorizontal = inputDevice.LeftStickX;
+		float moveVertical   = inputDevice.LeftStickY;
+
+		Debug.Log(">>>>" + moveVertical + "-" + moveHorizontal);
+
+		currentMovement =  new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.velocity = currentMovement * speed;
+		
+		rigidbody.position = new Vector3(
+			Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax),
+			0, 
+			Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
+			);
+	}
+}
---
+++ Assets/scripts/rotateTowardsStick.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+
+public class rotateTowardsStick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		var inputDevice = InputManager.ActiveDevice;
+
+		float rightStickX = inputDevice.RightStickX;
+		float rightStickY = inputDevice.RightStickY;
+
+		float yAngle = Mathf.Atan2(-rightStickX, rightStickY) * 180 / Mathf.PI;
+
+		if(yAngle == 0){
+			return;
+		}
+
+		transform.eulerAngles = new Vector3( 0, yAngle , 0 );
+	}
+}
